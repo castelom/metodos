87a3e51495eb7e82ef960ef7df2f1d8eef749580
Kat2 <katysombra@gmail.com>
2014-10-15 09:29:31 -0300

    Commit retirada de métodos

--- src/ifce/Contador.java
+++ src/ifce/Contador.java
@@ -22,19 +22,6 @@ public class Contador {
 			System.out.println(i);
 		}
 	}
-	
-	public void ContaTudo100(){
-		int i;
-		for(i = 0; i<= 100; i++){
-			System.out.println(i);
-		}
-	}
-	public void ContaTudo300(){
-		int i;
-		for(i = 0; i<= 300; i++){
-			System.out.println(i);
-		}
-	}
 	public void ContaParEImpar(int num){
 		System.out.println("Conta Pares");
 		ContaPares(num);
