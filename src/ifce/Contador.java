2eb5841c62a48f9e087c7adfb284b04850fa4215
Kat2 <katysombra@gmail.com>
2014-10-14 23:03:46 -0300

    primeiro commit

---
+++ src/ifce/Contador.java
@@ -0,0 +1,12 @@
+package ifce;
+
+public class Contador {
+	public String ContaPares(int num){
+		int i;
+		String texto = "números pares";
+		for(i = 0; i<= num; i = i+2){
+			System.out.println(i);
+		}
+		return texto;
+	}
+}
